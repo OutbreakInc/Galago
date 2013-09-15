af06ae89563a35c80cd74d87280039527a843602
Kuy Mainwaring <kuy@outbreak.co>
2013-09-15 13:22:53 -0700

    made bluetooth board example more interesting with bi-directional comms

--- Modules/examples/bluetoothBoard/main.cpp
+++ Modules/examples/bluetoothBoard/main.cpp
@@ -9,9 +9,11 @@ using namespace Logiblock::AppBoards;
 struct BlinkState
 {
 			BlinkState():
-				on(true)
+				on(true),
+				enabled(true)
 				{}
 	
+	bool	enabled;
 	bool	on;
 };
 
@@ -31,11 +33,45 @@ void	blinkLED(void* p, Task t = Task(), bool b = true)
 		blueBlock.enableBluetooth();
 	}
 	
-	state->on = !state->on;
+	if(state->enabled)
+		state->on = !state->on;
 	
 	io.serial.write("Test\n");
 	
 	system.when(system.delay(500), &blinkLED, p);
+}
+
+void onBytesReceived(void* p, Task t, bool success)
+{
+	BlinkState* state = (BlinkState*)p;
+	
+	bool prevState = state->on;
+	char c;
+	
+	//read bytes one-at-a-time and change control state accordingly
+	while(io.serial.read(&c, 1) > 0)
+	{
+		switch(c)
+		{
+		case '1':	// 1 enables blinking
+			state->enabled = true;
+			break;
+		case '0':	// 0 disables it
+			state->enabled = false;
+			break;
+		case 'x':	// x turns the LED on
+			state->on = true;
+			break;
+		case 'z':	// z turns it off
+			state->on = false;
+			break;
+		}
+	}
+	
+	if(prevState != state->on)
+		blueBlock.setLED(state->on);
+	
+	system.when(io.serial.bytesReceived(), &onBytesReceived, p);
 }
 
 int main(void)
@@ -46,6 +82,9 @@ int main(void)
 	
 	BlinkState state;
 	state.on = true;
+
+	system.when(io.serial.bytesReceived(), &onBytesReceived, &state);
+
 	blinkLED(&state);
 	
 	while(true)
