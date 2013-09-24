83b1a2f2d7e9a63ddc64dd6379d491d29ed4cef4
Kuy Mainwaring <kuy@outbreak.co>
2013-09-24 00:50:29 -0700

    css is now built by less; renamed docs to learn, added IDE link

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 echo Building in `pwd`...
-markup=`find * | grep -e "\.hbs\.html$" | grep -ve "^template/|^node_modules/"`
 
+markup=`find * | grep -e "\.hbs\.html$" | grep -ve "^template/"`
 for m in $markup
 do
 	out=`echo $m | sed 's/\.hbs//g'`
 	cat template/header.hbs.html $m template/footer.hbs.html > $out
-	echo built: $out
+	echo : built $out
 	git add $out
 done
+
+less=`find * | grep -e "\.less\.css$"`
+for css in $less
+do
+	out=`echo $css | sed 's/\.less//g'`
+	node_modules/.bin/lessc -x $css $out
+	echo : built $out
+	git add $out
+done
+
+echo Build complete
