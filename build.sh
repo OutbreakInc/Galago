014dcc8ed03515cdadd47fb97a725de06d0c9090
Kuy Mainwaring <kuy@outbreak.co>
2013-09-23 19:33:52 -0700

    updating build script

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 markup=`find * | grep -e "\.hbs\.html$" | grep -ve "^template/|^node_modules/"`
 
+echo Building in `pwd`...
 for m in $markup
 do
 	out=`echo $m | sed 's/\.hbs//g'`
 	cat template/header.hbs.html $m template/footer.hbs.html > $out
+	echo built: $out
 	git add $out
 done
