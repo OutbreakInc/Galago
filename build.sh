026fef9b54b1dcbe22cb57e694e9a24cfb2a95b0
Kuy Mainwaring <kuy@outbreak.co>
2013-11-28 03:01:46 -0800

    Kamikaze site checkin. Also I have a stomachache

--- build.sh
+++ build.sh
@@ -6,11 +6,38 @@ markup=`find * | grep -e "\.hbs\.html$" | grep -ve "^template/"`
 for m in $markup
 do
 	out=`echo $m | sed 's/\.hbs//g'`
-	cat template/header.hbs.html $m template/footer.hbs.html > $out
+	
+	# read params from template
+	t="template/header.hbs.html"
+	params=`grep "{{.*}}" $t | sed -E 's/.*{{(.*)}}.*/\1/g'`
+	
+	temp=`mktemp -t hbs`
+	cat template/header.hbs.html > $temp
+	
+	for p in $params
+	do
+		value=`grep "<!-- @meta.*$p" $m | sed -E 's/<!-- @meta (.*) -->/\1/g'`
+
+		#echo $value
+		if [ ! "$value" ]
+		then
+			value="{$p=\"\"}"
+		fi
+		
+		re=`echo $value | sed -E 's/\{\s*(.*)\s*=\s*"(.*)"\s*\}/s\/\{\{\1\}\}\/\2\/g/'`
+
+		#echo $re
+		sed -iE "$re" $temp
+	done
+
+	cat $temp $m template/footer.hbs.html > $out
+	rm $temp
+
 	echo : built $out
 	git add $out
 done
 
+echo Compiling less...
 less=`find * | grep -e "\.less\.css$"`
 for css in $less
 do
@@ -21,3 +48,10 @@ do
 done
 
 echo Build complete
+
+exit
+
+
+	# markdownify
+	grep -E "\|$" /Users/kuy/Projects/Galago/github/Modules/logiblock/galago/GalagoAPI.h | sed -E 's/^   (.*)\|$/\1/g' | sed -E 's/[ ]*$//g' | node_modules/.bin/md2html > learn/api/raw.html
+	
